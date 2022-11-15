1054b7094868b59d928f068b953e489a6959f5da
Antonina Kicha <antonina.kicha@gmail.com>
2022-11-15 22:10:05 +0200

    Добавляет решение задания1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body')
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let timerId = null;
+
+startBtn.addEventListener('click', changeColor);
+function changeColor() {
+    if (startBtn.classList.contains('onClick')) {
+        return;
+    }
+    timerId = setInterval(() => { body.style.backgroundColor = getRandomHexColor()}, 1000);
+    startBtn.classList.add('onClick')
+    console.log(timerId);
+}
+
+stoptBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.classList.remove('onClick')
+});
+
 
